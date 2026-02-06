1ebf1c60d1067dfc809664a61236c1e08466b7b6
Rameshmamuduru <rameshmamuduru7799@gmail.com>
2026-02-06 16:31:25 +0530

    Update App.js

--- hospital-3tier/frontend/src/App.js
+++ hospital-3tier/frontend/src/App.js
@@ -1,27 +1,32 @@
+import React, { useState } from "react";
 
-import React,{useEffect,useState} from "react";
+function App() {
 
-function App(){
+  const [patients, setPatients] = useState([]);
 
- const [patients,setPatients]=useState([]);
+  const getData = () => {
+    fetch("http://13.232.42.90:5000/patients")
+      .then(res => res.json())
+      .then(data => setPatients(data))
+      .catch(err => console.error(err));
+  };
 
- useEffect(()=>{
-   fetch("http://localhost:5000/patients")
-   .then(res=>res.json())
-   .then(data=>setPatients(data));
- },[]);
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hospital Management System</h1>
 
- return(
-  <div>
-   <h1>Hospital Management System</h1>
+      <button onClick={getData}>Get Data</button>
 
-   {patients.map(p=>(
-    <div key={p.id}>
-     {p.name} | {p.age} | {p.disease}
+      <br /><br />
+
+      {patients.map(p => (
+        <div key={p.id}>
+          {p.name} | {p.age} | {p.disease}
+        </div>
+      ))}
     </div>
-   ))}
-  </div>
- );
+  );
 }
 
 export default App;
+
